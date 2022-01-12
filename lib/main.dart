35839dd9f767dd02c322138c6d87a902910c767d
luke choi <primera_jh@hotmail.com>
2022-01-12 14:32:41 +0900

    seventh flutter app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("hello"),backgroundColor: Colors.white,),
+        body: Container(
+          color: Colors.redAccent,
+        ),
+      ),
+    );
+  }
+}
